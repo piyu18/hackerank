00793b28219e395eb1011c61ae4d6893080aa583
Priya <priya1803singh@gmail.com>
2022-10-05 23:46:03 +0530

    Add files via upload

---
+++ sql/BasicJoin/avg_population_each_continent.sql
@@ -0,0 +1,51 @@
+--Problem Statement
+/*
+Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+Input Format
+
+The CITY and COUNTRY tables are described as follows:
+City
++-------------+----------+
+| Field       | Type     |
++-------------+----------+
+| ID          | int(11)  |
+| Name        | char(35) |
+| CountryCode | char(3)  |
+| District    | char(20) |
+| Population  | int(11)  |
++-------------+----------+
+
+
+Country
++----------------+-------------+
+| Field          | Type        |
++----------------+-------------+
+| Code           | char(3)     |
+| Name           | char(52)    |
+| Continent      | char(50)    |
+| Region         | char(26)    |
+| SurfaceArea    | float(10,2) |
+| IndepYear      | smallint(6) |
+| Population     | int(11)     |
+| LifeExpectancy | float(3,1)  |
+| GNP            | float(10,2) |
+| GNPOld         | float(10,2) |
+| LocalName      | char(45)    |
+| GovernmentForm | char(45)    |
+| HeadOfState    | char(60)    |
+| Capital        | int(11)     |
+| Code2          | char(2)     |
++----------------+-------------+
+
+*/
+
+--Solution
+
+SELECT Country.Continent, FLOOR(AVG(City.Population))
+FROM Country
+INNER JOIN City
+ON Country.Code = City.CountryCode
+GROUP BY Country.Continent;
